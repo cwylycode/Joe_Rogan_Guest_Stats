4625a5328347a13dad87921b027894f9a8b34485
hexadeci-male <joeisnomoe@mail.com>
2021-05-22 23:14:33 -0600

    Prior to changing data structure

--- main.py
+++ main.py
@@ -8,40 +8,61 @@ import bs4
 import requests
 import json
 import re
+import os
 
-def update_guest_data():
+def update_guest_data(file_path:str,update_logs:bool=False,cnw_logs:bool=False):
     """
+    Collect Joe Rogan guest data and write to file with newest data from podcast using data sites and my CNW_Scraper tool's scrape_names function. Data is written out as klsjd;klfjfa
+
     Data came from jrelibrary.com and DataWrapper. There is so much junk in here - unicode identifiers for non-ascii guest names, extra backslashes, inconsistent naming and multiple guest conventions used, missing names, hyperlinks sprinkled everywhere, extra quotes and other characters, junk html, ugh...
+
+    The data is from the general podcasts ONLY - no MMA, fight companion, specials. Name and which episodes a guest appeared on are collected from jrelibrary, and the extra stuff (if avaliable) is from celebritynetworth. It's not perfect, but neither is the data that gets collected. So...Enjoy.
+
+    :file_path: String to where you want to save the guest data (should be saved as JSON, but whatever floats yer boat.) Will be overwritten if file already exists.
+
+    :update_logs: Print to terminal what this function is doing? False by default.
+
+    :cnw_logs: Set CNW verbose, console printing, and log file writing to true. Log file is written next to the file you set at file_path. Name is 'cnw.log' with date/time logging active. False by default.
+
+    :return: None.
     """
 
-    print("Updating guest data ...\n")
+    # Got a valid path for that file?
+    try:
+        with open(file_path,"w") as f:
+            pass
+    except:
+        raise ValueError("Ya dun goofed - file path invalid or inaccessible.")
+
+    if update_logs: print("Updating guest data ...\n")
 
     # If Rogan has any more guests on with funny names that only unicode can handle, add those chars here.
     uni_chars = {r"\u2019":"'",r"\u00E9":"e",r"\u00F1":"n"}
     url = "https://datawrapper.dwcdn.net/eoqPA/"
-    h = {"user-agent":"Young Jamie"} # LOL
+    usr_agt = "Young Jamie" # LOL
 
     # Perform html request for data
-    print("Getting raw data ...")
-    with requests.get(url=url,headers=h,timeout=10) as response:
+    if update_logs: print("Getting raw data ...")
+    with requests.get(url=url,headers={"user-agent":usr_agt},timeout=10) as response:
         response.raise_for_status()
         # Find latest data url link.
         url = re.search(r'(?<=url=).+?(?=")',response.text).group(0)
-        with requests.get(url=url,headers=h,timeout=10) as response:
+        with requests.get(url=url,headers={"user-agent":usr_agt},timeout=10) as response:
             response.raise_for_status()
             html = response.content
 
     # *cries in regex...and in unicode...and in bytes...and in backslashes*
-    print("Parsing raw data ...")
+    if update_logs: print("Parsing raw data ...")
     raw_script = bs4.BeautifulSoup(html,"html.parser").find_all("script")[1].contents[0]
     for k,v in uni_chars.items():
         raw_script = raw_script.replace(k,v)
     clean_script = raw_script.replace("\\","").replace("\"\"","\"")
     raw_entries = [l[0] for l in re.findall(r'((rn|">)#.+?\d{4}")',clean_script)]
+    # Entries are three parts: episode number, name(s) of guests, date of episode.
     entries = list(map(lambda x: x[3:].replace("</a>\"",""),raw_entries))
 
     # Create basic guest data from jrelibrary.com/datawrapper.
-    print("Setting up data objects ...")
+    if update_logs: print("Setting up data objects ...")
     guest_data = {}
     fix_exceptions = ["Dr. Phil","Mr. T"] # Add more if needed.
     fix_removal = ["Dr. ","Mr. ","Mrs. ","Ms. ","Cmdr. "] # Ditto.
@@ -71,15 +92,16 @@ def update_guest_data():
             guest_data.update({n:{"Name":n,"Appearances":[(ep_num,date)]}})
 
     # Get remaining data from celebritynetworth.com using my handy scraper.
-    print("Collecting extra data from CNW (this may take a bit) ...")
-    cnw.Options.custom_user_agent = "Young Jamie"
-    cnw.Logs.print_to_console = True
-    cnw.Logs.verbose = True
-    cnw.Logs.write_to_file(".data/test/testlog",False)
+    if update_logs: print("Collecting extra data from CNW (this may take a bit) ...")
+    cnw.Options.custom_user_agent = usr_agt
+    if cnw_logs:
+        cnw.Logs.print_to_console = True
+        cnw.Logs.verbose = True
+        cnw.Logs.write_to_file(os.path.split(file_path)[0]+"/cnw")
     profiles = cnw.scrape_names([n for n in guest_data.keys()])
 
     # Add extra data to the guests.
-    print("Parsing and adding extra data ...")
+    if update_logs: print("Parsing and adding extra data ...")
     valid_chars = lambda c: c.isalnum() or any([x in c for x in [" ","-","'"]])
     parse_name = lambda n: "".join(filter(valid_chars, n)).strip()
     for g in guest_data.keys():
@@ -97,23 +119,31 @@ def update_guest_data():
                 break
 
     # Write data and done.
-    print("Writing data to JSON file ...")
-    with open(".data/test/guest_data.json","w") as f:
+    if update_logs: print("Writing data to file ...")
+    with open(file_path,"w") as f:
         json.dump(guest_data,f,indent=4)
-    
-    print("\nGuest updates done.\n")
+    if update_logs: print("\nGuest updates done.\n")
 
-update_guest_data()
+# df = pandas.read_json(".data/test/guest_data.json")
 
-# def collect_wealth():
-    # Setup, get names, etc
-    # df:pandas.DataFrame = pandas.read_csv("data.csv")
-    # names:list = df["Name"].to_list()
-    # parser = {"thousand":1_000,"million":1_000_000,"billion":1_000_000_000}
+obj = """{
+        "Name": "Reggie Watts",
+        "Appearances": [
+            {"Episode": 1648,"Date": "May 8, 2021"},
+            {"Episode": 1484,"Date": "Jun 2, 2020"},
+            {"Episode": 1322,"Date": "Jul 10, 2019"},
+            {"Episode": 1015,"Date": "Sep 26, 2017"}
+        ],
+        "Net Worth": "2000000",
+        "Salary": "$666 per episode",
+        "Date of Birth": "Mar 23, 1972 (49 years old)",
+        "Gender": "Male",
+        "Height": "6 ft (1.83 m)",
+        "Profession": "Comedian, Singer, Musician, Actor, Screenwriter, Film Score Composer",
+        "Nationality": "United States of America",
+        "Last Updated": "2020"
+    }"""
 
-    # Attempt to turn the net worth into a real number
-    # try:
-    #     worth = worth.group(0)[1:].split()
-    #     worth = int(worth[0]) * parser[worth[1]]
-    # except:
-    #     print("ERROR: Couldn't parse the worth for some reason. Is the person a trillionaire, or something?!")
+# with open("test.json","w") as f:
+df = pandas.json_normalize(json.loads(obj))
+# print(df.dtypes)
